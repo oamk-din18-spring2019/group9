cbf6c0fa804cd4b54d402be6f44461a63f6d81be
julkuvii <julkunenviivi@gmail.com>
2019-03-27 14:01:04 +0200

    modified owner

--- application/controllers/Owner.php
+++ application/controllers/Owner.php
@@ -8,13 +8,16 @@ class Owner extends CI_Controller{
     parent::__construct();
     //Codeigniter : Write Less Do More
   }
+    function add_form(){
+      $data['page']='owners/add_form';
+      $this->load->view('menu/content',$data);
+    }
 
     function add_owner(){
-    $data['page']='owners/add_form';
-    $this->load->view('menu/content',$data);
+    $this->load->model('Owners_model');
     $hashedPassword=password_hash($this->input->post('password'),PASSWORD_DEFAULT);
 
-    insert_data=array(
+    $insert_data=array(
       "username"=>$this->input->post('username'),
       "password"=>$hashedPassword
     );
