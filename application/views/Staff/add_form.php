556f67755e8f2a5ea01d5f696dd3bbc1a4e3b68a
Your Name <you@example.com>
2019-03-29 09:47:54 +0200

    didi

---
+++ application/views/Staff/add_form.php
@@ -0,0 +1,15 @@
+<form class="" action="" method="post">
+  <label for="">staff ID</label> <br>
+  <input type="number" name="id" value=""> <br>
+
+  <label for="">Firstname</label> <br>
+  <input type="text" name="fn" value="" required> <br>
+
+  <label for="">Lastname</label> <br>
+  <input type="text" name="ln" value="" required> <br>
+
+  <label for="">username</label> <br>
+  <input type="text" name="em" value=""> <br>
+
+  <input type="submit" name="" value="Add">
+</form>
