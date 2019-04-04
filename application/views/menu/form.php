80d9082b4a93b2529ea85a7553f2a49d31277ad3
julkuvii <julkunenviivi@gmail.com>
2019-04-04 15:06:37 +0300

    the buttons should work now maybe perhaps

--- application/views/menu/form.php
+++ application/views/menu/form.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>RESERVATION</title>
   <!--  <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/form.css'); ?>"> -->
-
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 
@@ -39,68 +39,69 @@
             <label for="date">Check-out date</label>
             <input type="date" name="date" min="2019-01-01">
             <br>
+        <div class="">
+          <?php     echo '<a href="'.site_url('').'"><span class="btn">Search</btn> </a>';
+                    echo" <br>";
 
-            <?php     echo("<button onclick=\"location.href=''\">Search</button>");
-                      echo" <br>";
-                      echo("<button onclick=\"location.href='owner/add_owner_user_form'\">Book now</button>");
-                      echo" <br>";
-            ?>
+                    echo '<a href="'.site_url('owner/add_owner_user_form').'"><span class="btn">Book now</btn> </a>';
+                    echo" <br>";
+          ?>
+          <hr>
+        </div>
+
         </fieldset>
         <br>
-<?php
-if (($_SESSION['logged_in'] == true)) {
-      if ($_SESSION['owner_logged_in'] == true) {
-        echo("<button onclick=\"location.href=''\">Make new reservation</button>");
-        echo" <br>";
-        echo("<button onclick=\"location.href='login_owner/logout'\">Owner Logout</button>");
-        echo "<br>";
+    <div class="">
+      <?php
+      if (($_SESSION['logged_in'] == true)) {
+            if ($_SESSION['owner_logged_in'] == true) {
+              echo '<a href="'.site_url('').'"><span class="btn">Make new reservation</btn> </a>';
+              echo" <br>";
 
+              echo '<a href="'.site_url('login_owner/logout').'"><span class="btn">Owner Logout</btn> </a>';
+              echo "<br>";
+
+            }
+
+
+          //
+
+            else if ($_SESSION['staff_logged_in'] == true) {
+
+              echo '<a href="'.site_url('staff/show_staff').'"><span class="btn">view staff</btn> </a>';
+              echo "<br>";
+
+              echo '<a href="'.site_url('owner/show_owner').'"><span class="btn">view owners</btn> </a>';
+              echo "<br>";
+
+              echo '<a href="'.site_url('').'"><span class="btn">view animals</btn> </a>';
+              echo "<br>";
+
+              echo '<a href="'.site_url('').'"><span class="btn">view stays</btn> </a>';
+              echo "<br>";
+
+              echo '<a href="'.site_url('login_staff/logout').'"><span class="btn">Staff Logout</btn> </a>';
+              echo "<br>";
+            }
       }
 
+      else {
 
-    //
 
-      else if ($_SESSION['staff_logged_in'] == true) {
-        echo("<button onclick=\"location.href='staff/show_staff'\">view staff</button>");
+        echo '<a href="'.site_url('owner/add_owner_user_form').'"><span class="btn">Create a new account</btn> </a>';
         echo" <br>";
 
-        echo("<button onclick=\"location.href='owner/show_owner'\">view owners</button>");
+        echo '<a href="'.site_url('staff/add_staff_user_form').'"><span class="btn">Add staff user</btn> </a>';
         echo" <br>";
 
-        echo("<button onclick=\"location.href=''\">view animals</button>");
+        echo '<a href="'.site_url('login_owner/login').'"><span class="btn"> Owner login</btn> </a>';
         echo" <br>";
 
-        echo("<button onclick=\"location.href=''\">view stays</button>");
+        echo '<a href="'.site_url('login_staff/login').'"><span class="btn">Staff Login</btn> </a>';
         echo" <br>";
-
-        echo("<button onclick=\"location.href='login_staff/logout'\">Staff Logout</button>");
-        echo "<br>";
       }
-}
-
-else {
-
-
-  echo("<button onclick=\"location.href='owner/add_owner_user_form'\">Create a new account</button>");
-  echo" <br>";
-
-  echo("<button onclick=\"location.href='staff/add_staff_user_form'\">Add staff user</button>");
-  echo" <br>";
-
-  echo("<button onclick=\"location.href='login_owner/login'\">Owner Login</button>");
-  echo" <br>";
-
-  echo("<button onclick=\"location.href='login_staff/login'\">Staff Login</button>");
-  echo" <br>";
-}
-
-
-
-
-
-
-?>
-
+      ?>
+    </div>
     </form>
 <hr>
 <hr>
