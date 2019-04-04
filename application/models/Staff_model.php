97167766030f34d4c8aca47ea446540f2bf40c72
didiiben <dikrasadeq96@gmail.com>
2019-04-04 10:28:37 +0300

    before pull

--- application/models/Staff_model.php
+++ application/models/Staff_model.php
@@ -9,12 +9,11 @@ class Staff_model extends CI_Model{
     //Codeigniter : Write Less Do More
   }
   public function getStaff(){
-    $this->db->select('idStaff,fname,lname,username,');
+    $this->db->select('staff_id,staff_fname,staff_lname,staff_username,staff_password');
     $this->db->from('staff');
-    $this->db->
     return $this->db->get()->result_array();
   }
-  public function add_staff($insert_data){
-    $this->db->insert('staff',$insert_data);
-    return $this->db->affected_rows();
-  }
+  
+
+}
+?>
