54591bb6d52b92f395e34a19755d5faed259596d
julkuvii <julkunenviivi@gmail.com>
2019-04-05 11:43:48 +0300

    before pull

---
+++ application/views/owners/add_owner_user_form.php
@@ -0,0 +1,22 @@
+
+<h3>Create account</h3>
+
+<p>
+<form class="" action="<?php  echo site_url('owner/add_owner_user'); ?>" method="post">
+<label for="">ID</label> <br>
+<input type="hidden" name="owner_id" value="<?php echo $new_id; ?>"> <br>
+<label for="">Firstname</label><br>
+<input type="text" name="owner_fname" value=""><br>
+<label for="">Lastname</label><br>
+<input type="text" name="owner_lname" value=""><br>
+<label for="">Phone number</label><br>
+<input type="text" name="owner_phone" value=""><br>
+<label for="">Username</label> <br>
+<input type="text" name="owner_username" value=""> <br>
+<label for="">Password</label> <br>
+<input type="text" name="owner_password" value=""> <br>
+<label for="">Date of birth</label> <br>
+<input type="date" name="owner_birthday" value=""> <br><br>
+<input type="submit" name="" value="Add">
+</form>
+</p>
---
+++ application/views/staff/add_staff_user_form.php
@@ -0,0 +1,20 @@
+
+<h3>Add staff user</h3>
+
+<p>
+<form class="" action="<?php  echo site_url('staff/add_staff_user'); ?>" method="post">
+<label for="">ID</label> <br>
+<input type="hidden" name="staff_id" value="<?php echo $new_staff_id; ?>"> <br>
+<label for="">Firstname</label><br>
+<input type="text" name="staff_fname" value=""><br>
+<label for="">Lastname</label><br>
+<input type="text" name="staff_lname" value=""><br>
+<label for="">Username</label> <br>
+<input type="text" name="staff_username" value=""> <br>
+<label for="">Password</label> <br>
+<input type="text" name="staff_password" value=""> <br>
+<label for="">Hours</label> <br>
+<input type="number" name="staff_hours" value=""> <br><br>
+<input type="submit" name="" value="Add">
+</form>
+</p>
