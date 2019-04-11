82a67aebaeb0d12d773a538d33cfa4b591c5efbf
Tiina <t7mati01@students.oamk.fi>
2019-04-11 14:15:00 +0300

    problem with stay model

--- application/models/Stay_model.php
+++ application/models/Stay_model.php
@@ -1,26 +1,22 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 class Stay_model extends CI_Model
 {
 
-  public function __construct()
-  {
-    parent::__construct();
-    //Codeigniter : Write Less Do More
-  }
   public function getStay()
   {
       $this->db->select('*');
       $this->db->from('stays');
       return $this->db->get()->result_array();
   }
+
   public function add_stay($insert_data)
   {
-    $this->db->insert('Stay',$insert_data);
+    $this->db->insert('stays',$insert_data);
     return $this->db->affected_rows();
   }
+
   public function get_chosen($id)
   {
       $this->db->select('*');
